a429dab655e56391b1f375838bce5affad3261f5
vmalaviya <varunmalaviya7@gmail.com>
2023-10-17 13:10:37 -0400

    added navbar and react-link-smooth-scroll as a dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,27 @@
+'use client'
 import Hero from "./components/Hero"
 import About from "./components/About"
 import Portfolio from "./components/Portfolio"
 import Contact from "./components/Contact"
+import NavBar from "./components/Navbar"
+import { useState } from 'react'
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col">
-        <Hero />
-        <About />
-        <Portfolio />
-        <Contact />
-        Blog
-    </main>
-  )
+    //using state to identify the current page the user is browser
+    const [ currentPage, setCurrentPage ] = useState<string>('home')
+
+    return (
+        <main className="flex min-h-screen flex-col">
+            <NavBar 
+                page='N/A' 
+                currentPage={currentPage} 
+                setCurrentPage={setCurrentPage}
+            />
+            <Hero />
+            <About />
+            <Portfolio />
+            <Contact />
+            Blog
+        </main>
+    )
 }
